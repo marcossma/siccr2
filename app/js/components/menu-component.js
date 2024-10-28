ff59101aa1afbce24e33ed7e27460d9eee5d0faa
Marcos Silva <marcossma@gmail.com>
2024-10-28 10:00:37 -0300

    alteração submenu open

--- app/js/components/menu-component.js
+++ app/js/components/menu-component.js
@@ -10,9 +10,16 @@ class MenuComponent extends HTMLElement {
         // Código do HTML e CSS no shadow DOM
         this.shadowRoot.innerHTML = `
             <style>
+                * {
+                    margin: 0px;
+                    padding: 0px;
+                    box-sizing: border-box;
+                    font-family: verdana, sans serif;
+                }
+
                 nav {
-                    background: #007bff;
-                    padding: 10px;
+                    background: none;
+                    // padding: 10px;
                 }
 
                 nav ul {
@@ -21,29 +28,45 @@ class MenuComponent extends HTMLElement {
                     padding: 0px;
                     display: flex;
                     flex-direction: column;
-                    gap: 20px;
+                    gap: 10px;
                 }
 
                 nav ul li {
-                    color: white;
+                    border-radius: 5px;
+                    // background: #009536;
+                    border: 1px solid #009536;
+                    color: #009536;
                     text-decoration: none;
                     font-weight: bold;
-                    padding: 10px;
+                    font-size: 12px;
+                    padding: 5px;
                     cursor: pointer;
-                    transition: 0.3s;
+                    transition: 0.3s ease-in-out;
                 }
 
                 nav ul li:hover {
-                    background: silver;
+                    background: #009536;
+                    color: white;
                 }
 
-                nav ul li:hover .submenu {
-                    // height: auto;
-                    transiton: 0.3s;
+                nav ul li p {
+                    padding: 5px;
+                    font-size: 10px;
+                }
+
+                nav ul li p:hover {
+                    text-decoration: underline;
                 }
 
                 .submenu {
-                    height: 0px;
+                    max-height: 0px;
+                    overflow: hidden;
+                    position: relative;
+                    transition: 0.3s ease;
+                }
+
+                .submenu.open {
+                    max-height: 200px;
                 }
             </style>
             
@@ -53,14 +76,40 @@ class MenuComponent extends HTMLElement {
                     <li>Sobre
                         <div class="submenu">
                             <p>Este é o submenu</p>
+                            <p>Este é o submenu</p>
+                            <p>Este é o submenu</p>
                         </div>
                     </li>
-                    <li>Serviços</li>
+                    <li>Serviços
+                        <div class="submenu">
+                            <p>Este é o submenu</p>
+                            <p>Este é o submenu</p>
+                            <p>Este é o submenu</p>
+                        </div>
+                    </li>
                     <li>Contato</li>
                 </ul>
             </nav>
         `;
-    }
+
+        const menuItems = this.shadowRoot.querySelectorAll("nav ul li");
+
+        menuItems.forEach(menu => {
+            menu.addEventListener("click", () => {
+                // Esconcer os todos os submenus antes de abrir o menu atual
+                this.shadowRoot.querySelectorAll(".submenu").forEach(submenu => {
+                    submenu.classList.remove("open");
+                });
+
+                // // Alternar o submenu do item atual
+                const submenu = menu.querySelector(".submenu");
+                if (submenu) {
+                    submenu.classList.toggle("open");
+                }
+            });
+        });
+
+    } // Fim connectedCallback()
 }
 
 // Registrar o componente
