c3ae4f1260ad004db8942ad0d42c83533602ef02
Marcos <marcossma@gmail.com>
2025-04-02 15:02:25 -0300

    painel adm unidades

--- api/public/adm/js/script.js
+++ api/public/adm/js/script.js
@@ -1,24 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("Módulo Administrativo carregado.");
 
-    // Seleção de elementos
-    const btnAdicionar = document.querySelector(".btn_adicionar");
-    const frmUnidade = document.querySelector(".frmUnidade");
-    const btnCadastrarUnidade = document.querySelector(".cadastrarUnidade");
-    const btnCancelarUnidade = document.querySelector(".cancelarUnidade");
-    const dialogPainel = document.querySelector(".dialogPainel");
-
-    // Adição de Listeners
-    btnAdicionar.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (event.target.classList.contains("unidade")) {
-            dialogPainel.showModal();
-        }
-    });
-
-    btnCancelarUnidade.addEventListener("click", function(event) {
-        event.preventDefault();
-        frmUnidade.reset();
-        dialogPainel.close();
-    });
+    if (window.location.href.includes("unidade")) {
+        // Seleção de elementos
+        const btnAdicionar = document.querySelector(".btn_adicionar");
+        const frmUnidade = document.querySelector(".frmUnidade");
+        const btnCadastrarUnidade = document.querySelector(".cadastrarUnidade");
+        const btnCancelarUnidade = document.querySelector(".cancelarUnidade");
+        const dialogPainel = document.querySelector(".dialogPainel");
+        
+        // Adição de Listeners
+        btnAdicionar.addEventListener("click", function(event) {
+            event.preventDefault();
+            if (event.target.classList.contains("unidade")) {
+                dialogPainel.showModal();
+            }
+        });
+        
+        btnCancelarUnidade.addEventListener("click", function(event) {
+            event.preventDefault();
+            frmUnidade.reset();
+            dialogPainel.close();
+        });
+    }
+    
 });
